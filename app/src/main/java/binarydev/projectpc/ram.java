2a4672d9b227bd4524345582b2a48dfc1e6f11ec
rameetss <rameet.sekhon@uoit.net>
2017-11-12 22:19:26 -0500

    Added intents
    Started adding intents so user can switch between pages from navigation bar. Only RAM page implemented so far.

---
+++ app/src/main/java/binarydev/projectpc/ram.java
@@ -0,0 +1,31 @@
+package binarydev.projectpc;
+
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by Rameet on 11/12/2017.
+ */
+
+public class ram extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ram);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+
+    }
+}
