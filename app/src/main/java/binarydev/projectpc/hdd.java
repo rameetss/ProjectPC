6b278719792f4f10017fc05b3b53c77696bf9438
rameetss <rameet.sekhon@uoit.net>
2017-11-13 20:55:17 -0500

    new color scheme
    Just messing around with the colour scheme.  Added floating action button to all pages

--- app/src/main/java/binarydev/projectpc/hdd.java
+++ app/src/main/java/binarydev/projectpc/hdd.java
@@ -2,13 +2,16 @@ package binarydev.projectpc;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
 /**
  * Created by Rameet on 11/12/2017.
@@ -23,6 +26,15 @@ public class hdd extends AppCompatActivity
         setContentView(R.layout.activity_hdd);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
