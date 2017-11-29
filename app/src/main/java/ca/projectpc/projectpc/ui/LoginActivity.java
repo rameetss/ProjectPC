37568fb413a4d2bf0aaa0b9a7664eef3021875d0
Thagshan <thagshan18@gmail.com>
2017-11-28 20:33:39 -0500

    Rough Login Layout

---
+++ app/src/main/java/ca/projectpc/projectpc/ui/LoginActivity.java
@@ -0,0 +1,19 @@
+package ca.projectpc.projectpc.ui;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import ca.projectpc.projectpc.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+}
+
+
