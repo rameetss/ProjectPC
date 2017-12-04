f966f3dc077b019073c55c7ff0c429ed418af137
jesse-crouch <xycrouchwb@gmail.com>
2017-12-03 20:42:32 -0500

    MessageActivity and related layout xml. Drawable xml designs included.

---
+++ app/src/main/java/ca/projectpc/projectpc/ui/MessageActivity.java
@@ -0,0 +1,19 @@
+package ca.projectpc.projectpc.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ca.projectpc.projectpc.R;
+
+public class MessageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+
+        // Enable back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
+}
